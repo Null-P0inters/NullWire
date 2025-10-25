a9fc9d14e93446ccf1cf4a25d5d1a256cd594ee5
Nilanjan Mondal <nilanjanmondal165@gmail.com>
2025-10-25 20:21:39 +0530

    [FIX]: fix cors policy

--- server/index.js
+++ server/index.js
@@ -7,7 +7,11 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "*" }));
+app.use(cors({
+    origin: "*",
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
 
 app.use("/", firmwareRoutes);
 
