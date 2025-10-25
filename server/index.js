27d5d8b7c8ec3e0c408051d4d635e8e051dca037
Nilanjan Mondal <nilanjanmondal165@gmail.com>
2025-10-25 22:50:05 +0530

    [FIX]: fix json parser

--- server/index.js
+++ server/index.js
@@ -6,7 +6,10 @@ import cors from "cors";
 dotenv.config();
 
 const app = express();
+app.use(express.text());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(cors({
     origin: "*",
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
